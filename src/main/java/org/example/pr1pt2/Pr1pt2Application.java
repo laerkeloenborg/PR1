0a4df95fccf545a80a148651ffc5cc66f8694a44
laerkeloenborg <lalo0001@stud.kea.dk>
2025-02-18 10:20:33 +0100

    hello

---
+++ src/main/java/org/example/pr1pt2/Pr1pt2Application.java
@@ -0,0 +1,13 @@
+package org.example.pr1pt2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pr1pt2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pr1pt2Application.class, args);
+    }
+
+}
